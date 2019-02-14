a298bc23082f359ae95b200306b5e4170873d088
mysuarez97 <myrlene97suarez@gmail.com>
2019-02-13 23:45:30 -0500

    Add files via upload

--- userInterface.java
+++ userInterface.java
@@ -5,25 +5,25 @@ public class userInterface {
 
     private static void userWindow()
     {
-        JFrame frame = new JFrame("Search Engine");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-
-        JLabel textLabel = new JLabel("Search Engine");
-        textLabel.setLocation(100,60);
-        textLabel.setPreferredSize(new Dimension(500,500));
-
-
-        frame.getContentPane().add(textLabel,BorderLayout.CENTER);
-
-        frame.setLocationRelativeTo(null);
-        frame.pack();
-        frame.setVisible(true);
+        //sets up the jframe window
+        JFrame textLabel=new JFrame("Search Engine");
+        //title of the window and subtitle
+        JLabel title,subtitle;
+        title=new JLabel("Search Engine");
+        title.setBounds(100,10, 300,50);
+        title.setFont(new Font("Arial", Font.BOLD,36));
+        subtitle=new JLabel("Search Terms:");
+        subtitle.setBounds(50,70, 100,30);
+        textLabel.add(title); textLabel.add(subtitle);
+        textLabel.setSize(500,600);
+        textLabel.setLayout(null);
+        textLabel.setVisible(true);
 
     }
 
     public static void main(String[] args)
     {
+        //outputs the user window
         userWindow();
     }
 
